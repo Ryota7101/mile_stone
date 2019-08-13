a18d9767ff7b21c5ae0ad73231522a044cd1fbf5
Ryota Suzuki <ryotasuzuki.com@gmail.com>
2019-08-13 08:00:21 +0000

    stripe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190813042432) do
+ActiveRecord::Schema.define(version: 20190813075012) do
 
   create_table "members", force: :cascade do |t|
     t.integer "tenant_id"
