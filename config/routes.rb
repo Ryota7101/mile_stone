6ef2f23de257c9c5b3dd596a3e6ebec8df2c4ae4
Ryota Suzuki <ryotasuzuki.com@gmail.com>
2019-08-15 01:41:32 +0000

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, :controllers => { 
-    :registrations => "registrations",
+    :registrations => "milia/registrations",
     :confirmations => "confirmations",
     :sessions => "milia/sessions", 
     :passwords => "milia/passwords", 
